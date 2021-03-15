f53a2ce03cbe75a754a7e2a977517466292322de
betta <betta@DESKTOP-LJQJ287>
2021-03-15 19:20:00 +0100

    Modul3Nivell3

--- src/Modul3Nivell3.java
+++ src/Modul3Nivell3.java
@@ -6,9 +6,9 @@ public class Modul3Nivell3 {
 		Scanner introdueix = new Scanner(System.in);
 		int inici=0, a=1, n, num;
 		
-		System.out.println("Intrduce el nuemro: ");
+		System.out.println("Introdueix un número : ");
 		num=introdueix.nextInt();
-		System.out.println("La secuencia del fibonacci del número "+num+" és:");
+		System.out.println("La seqüència del fibonacci del número "+num+" és:");
 		for(int i=0;i<num;i++) {
 			System.out.println(inici);
 			n = inici+a;
